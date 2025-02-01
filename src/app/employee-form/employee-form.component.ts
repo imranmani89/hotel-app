d4c1921bb52e2f54c5fbdc37e4343c6d3f76a4f6
Imran <imran_gcs@hotmail.com>
2025-02-01 14:19:47 +0500

    resolved errors

--- src/app/employee-form/employee-form.component.ts
+++ src/app/employee-form/employee-form.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.css']
 })
-export class EmployeeFormComponent {
-employeeForm: FormGroup = new FormGroup({});
+export class EmployeeFormComponent implements OnInit {
+  employeeForm: FormGroup = new FormGroup({});
 
-constructor(){}
+  constructor(private formBuilder: FormBuilder) { }
 
-onSubmit(): void{
+  ngOnInit(): void {
+    this.employeeForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      fatherName: ['', Validators.required],
+      DOB: ['', Validators.required],
+      isActive: new FormControl()
+    });
+  }
 
-}
+  onSubmit(): void {
+    if (this.employeeForm.valid) {
+      alert("hello moi boy");
+    }
+  }
 }
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmployeeService } from './employee.service';
 import { EmployeeListComponent } from '../employee-list/employee-list.component';
 import { HomeModule } from "../home/home.module";
@@ -15,8 +15,9 @@ import { EmployeeFormComponent } from '../employee-form/employee-form.component'
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule, //for form validations and binding
     HomeModule
-],
+  ],
   providers: [
     EmployeeService
   ]
